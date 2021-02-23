af20c8966c02c519a7c2de7acff396858b7928c4
Evgeny <oigenstukov@gmail.com>
2021-02-23 21:32:43 +0300

    rolling back

--- lesson07_java_collections/main_task/src/main/java/org/example/almostExceptions/WrongItemException.java
+++
@@ -1,7 +0,0 @@
-package org.example.almostExceptions;
-
-public class WrongItemException {
-    public static void message() {
-        System.err.println("Wrong item. Choose another...");
-    }
-}
--- lesson07_java_collections/main_task/src/main/java/org/example/enums/Type.java
+++
@@ -1,8 +0,0 @@
-package org.example.enums;
-
-public enum Type {
-    ECONOMY,
-    COMFORT,
-    BUSINESS,
-    CARGO
-}
